491aa1e15b68aeb1de68de81a20c8ecee37c6f38
hayalows <mensahpkay+github@gmail.com>
2026-06-14 14:57:47 +0000

    Add Match Hub page

---
+++ pages/4_Match_Hub.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+from features.match_hub import inject_styles, render_match_hub
+from features.product_ui import render_project_footer, render_specialist_sidebar
+
+st.set_page_config(
+    page_title="CupMarket Match Hub",
+    page_icon="⚽",
+    layout="wide",
+    initial_sidebar_state="auto",
+)
+
+inject_styles()
+render_specialist_sidebar("match_hub")
+render_match_hub()
+render_project_footer()
